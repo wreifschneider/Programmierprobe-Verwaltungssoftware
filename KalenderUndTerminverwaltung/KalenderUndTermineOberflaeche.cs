0455ecef912137c7fcc1f9f3a3e39dbcc03fc7cf
Willy Reifschneider <reifschneider.w@outlook.com>
2023-08-29 14:25:35 +0200

    Code optimisation

--- KalenderUndTerminverwaltung/KalenderUndTermineOberflaeche.cs
+++ KalenderUndTerminverwaltung/KalenderUndTermineOberflaeche.cs
@@ -8,17 +8,20 @@ namespace KalenderUndTerminverwaltung
 {
     internal class KalenderUndTermineOberflaeche
     {
+        // Felder
+
+        private static bool falscheEingabe = false;
+        private static string blue = "\u001b[34m", red = "\u001b[31m", green = "\u001b[32m", yellow = "\u001b[33m",
+            magenta = "\u001b[35m", cyan = "\u001b[36m", darkGray = "\u001b[90m", reset = "\u001b[0m";
+
         // Kalender des ausgewählten Monats wird generiert 
         public static void KalenderAusdruck()
         {
             Kalender.KalenderMonatGenerierung();
             Termine.TermineTageZurListAuslesung();
 
-            Console.OutputEncoding = System.Text.Encoding.UTF8; // UTF-8 wird gebraucht um ein Zeichen anzeigen zu können.
+            Console.OutputEncoding = Encoding.UTF8; // UTF-8 wird gebraucht um ein Zeichen anzeigen zu können.
 
-            Console.WriteLine($"\n {Kalender.Jahr} {Kalender.MonatAusgabe()}");
-            //Console.SetCursorPosition(11, 1);
-            //Console.Write($"{Kalender.Jahr}");
             Console.SetCursorPosition(18, 1);
             Console.Write("Mo Di Mi Do Fr Sa So\n\t          ____________________\n");
             for (int i = 0; i < Kalender.KalendertageTabelle.GetLength(0); i++)
@@ -36,12 +39,11 @@ namespace KalenderUndTerminverwaltung
                         tag = 0;
                     }
 
-
                     for (int k = 0; k < Termine.TageVergangernerTermine.Count; k++)
                     {
                         if (tag == Termine.TageVergangernerTermine[k])
                         {
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.Write(darkGray);
                             break;
                         }
                     }
@@ -49,292 +51,252 @@ namespace KalenderUndTerminverwaltung
                     {
                         if (tag == Termine.TageZukuenftigerTermine[k])
                         {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(yellow);
                             break;
                         }
                     }
 
                     if (tag == Termine.TagHeutigerTermine && Kalender.MonatUndJahrAktuell())
-                    {
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                    }
+                        Console.Write(cyan);
                     else if (tag == DateTime.Today.Day && Kalender.MonatUndJahrAktuell())
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                    }
+                        Console.Write(red);
 
                     if (tag < 10)
-                    {
                         Console.Write($" {Kalender.KalendertageTabelle[i, j]} ");
-                    }
                     else
-                    {
                         Console.Write($"{Kalender.KalendertageTabelle[i, j]} ");
-                    }
 
-                    Console.ResetColor();
+                    Console.Write(reset);
                 }
                 Console.WriteLine("\b|");
             }
             Console.WriteLine("\t          ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+
+            Console.SetCursorPosition(0, 8);
+            Console.WriteLine($" {Kalender.Jahr} {Kalender.MonatAusgabe()}");
         }
 
         // Menüs:
+        // Menü für die Auswahl des Monats und Jahres
+        static void DatumEingabeMenue()
+        {
+            int eingabeMonat;
+            int eingabeJahr;
+            falscheEingabe = false;
+            do
+            {
+                falscheEingabe = true;
+                Console.Clear();
+                KalenderAusdruck();
+
+                Console.WriteLine("\n\n Monat  (mm):\n\n Jahr (jjjj):");
+
+                Console.SetCursorPosition(14, 11);
+                eingabeMonat = IntPruefer();
+
+                if (eingabeMonat > 0 && eingabeMonat <= 12)
+                {
+                    Kalender.Monat = eingabeMonat;
+
+                    eingabeJahr = IntPruefer();
+
+                    if (eingabeJahr > 0)
+                    {
+                        Kalender.Jahr = eingabeJahr;
+
+                        falscheEingabe = false;
+                    }
+                }
+
+                if (falscheEingabe)
+                {
+                    FalscheEingabe(18, 15, 1);
+                }
+
+            } while (falscheEingabe);
+        }
+
         // Das ist das eigentlcihe Hauptmenü aus der aus man in verschiedene andere Menüs navigieren kann.
         public static void KalenderMenue()
         {
-            string? eingabe;
             do
             {
                 Console.Clear();
+                Console.CursorVisible = false;
                 KalenderAusdruck();
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("\n        (V)orheriger  (A)ktueller  (N)ächster");
-                Console.ResetColor();
-                Console.Write("\n\n\t :\n\n ( )ermine:\n\n");
+                Console.Write($"{blue}\n      (V)Vorheriger   (A)Aktueller   (N)Nächster{reset}");
+                Console.Write($"\n\n\t :\n\n ({yellow}T{reset})Termine:\n\n");
                 Termine.TermineListeAusgabe(false);
 
-                Console.SetCursorPosition(2, 15);
-                Console.ForegroundColor= ConsoleColor.Yellow;
-                Console.Write("T");
-
-                Console.SetCursorPosition(1, 3);
-                Console.ForegroundColor= ConsoleColor.Green;
-                Console.Write("(K)alender");
+                Console.SetCursorPosition(1, 1);
+                Console.Write($"{blue}({green}D{blue})Datum");
 
                 Console.SetCursorPosition(55, 1);
-                Console.ForegroundColor= ConsoleColor.Blue;
-                Console.Write("( )Beenden");
+                Console.Write($"{blue}({red}X{blue})Beenden");
+                Console.Write(reset);
 
-                Console.SetCursorPosition(56, 1);
-                Console.ForegroundColor= ConsoleColor.Red;
-                Console.Write("X");
-                Console.ResetColor();
+                Console.SetCursorPosition(11, 13); 
 
-                Console.SetCursorPosition(11, 13);
-                eingabe = Console.ReadLine();
-
-                if (eingabe != null) eingabe = eingabe.ToLower().Trim();
-
-                switch (eingabe)
+                switch (Console.ReadKey(true).Key)
                 {
-                    case "n":
+                    case ConsoleKey.N:
                         Kalender.NaechsterMonat();
                         break;
-                    case "v":
+                    case ConsoleKey.V:
                         Kalender.VorherigerMonat();
                         break;
-                    case "a":
+                    case ConsoleKey.A:
                         Kalender.AktuellerMonat();
                         break;
-                    case "k":
-                        MonatUndJahrEingabeMenue();
+                    case ConsoleKey.D:
+                        Console.CursorVisible = true;
+                        DatumEingabeMenue();
                         break;
-                    case "t":
+                    case ConsoleKey.T:
                         TerminMenue();
                         break;
-                    case "x":
-                        Console.SetCursorPosition(0, 30);
+                    case ConsoleKey.X:
                         return;
                     default:
-                        HilfeText(3, 10);
-                        FalscheEingabe(18, 15, 3);
+                        Console.WriteLine("404 somethimhg get wrong");
                         break;
                 }
             } while (true);
         }
-
-        // Menü für die Auswahl des Monats und Jahres
-        static void MonatUndJahrEingabeMenue()
+        private static int IntPruefer()
         {
-            int eingabeMonat;
-            int eingabeJahr;
-            bool falscheiEingabe = false;
-            do
-            {
-                if (falscheiEingabe)
-                {
-                    FalscheEingabe(18, 13, 1);
-                }
-                falscheiEingabe = false;
-                Console.Clear();
-                KalenderAusdruck();
-
-                Console.WriteLine("\n Monat  (mm):\n\n Jahr (jjjj):");
-
-
-                try
-                {
-                    Console.SetCursorPosition(14, 11);
-                    eingabeMonat = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    eingabeMonat = -1;
-                    falscheiEingabe = true;
-                }
-
-
-                if (!falscheiEingabe && eingabeMonat > 0 && eingabeMonat <= 12)
-                {
-                    Kalender.Monat = eingabeMonat;
-                }
-                else
-                {
-                    falscheiEingabe = true;
-                }
-
-                if (falscheiEingabe) continue;
-                try
-                {
-                    Console.SetCursorPosition(16, 13);
-                    eingabeJahr = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    eingabeJahr = -1;
-                    falscheiEingabe = true;
-                }
-
-                if (!falscheiEingabe && eingabeJahr > 0)
-                {
-                    Kalender.Jahr = eingabeJahr;
-                }
-                else
-                {
-                    falscheiEingabe = true;
-                }
-
-            } while (falscheiEingabe);
+            return (int.TryParse(Console.ReadLine(), out int i) ? i : -1);
         }
+
 
         // Das Hauptmenü für Termine aus der man verschiedene Menüs für Termine ansteurn kann.
         static void TerminMenue()
         {
-            string? eingabe;
             do
             {
                 Console.Clear();
                 KalenderAusdruck();
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("\n          (H)inzufügen (B)earbeiten (L)öschen ");
-                Console.ResetColor();
+                Console.Write($"{yellow}\n          (H)Hinzufügen (B)Bearbeiten (L)Löschen{reset}");
 
                 Console.Write("\n\n\t : \n\n Termine:\n\n");
                 Termine.TermineListeAusgabe(false);
 
                 Console.SetCursorPosition(55, 1);
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("(Z)urück");
-                Console.ResetColor();
+                Console.Write($"{blue}(Z)Zurück{reset}");
 
-                Console.SetCursorPosition(11, 13);
-                eingabe = Console.ReadLine();
-
-                if (eingabe != null) eingabe = eingabe.ToLower().Trim();
-
-                switch (eingabe)
+                switch (Console.ReadKey(true).Key)
                 {
-                    case "h":
+                    case ConsoleKey.H:
+                        Console.CursorVisible = true;
                         TermineEingabeMenue(-1, 'h');
                         break;
-                    case "l":
+                    case ConsoleKey.L:
                         TermineIDMenue(false);
                         break;
-                    case "b":
+                    case ConsoleKey.B:
                         TermineIDMenue(true);
                         break;
-                    case "z":
+                    case ConsoleKey.Z:
                         return;
                     default:
-                        HilfeText(3, 10);
-                        FalscheEingabe(18, 15, 3);
+                        Console.WriteLine("404 something get wrong");
                         break;
                 }
 
             } while (true);
         }
 
+        static void IDBearbeitung(bool bearbeiten, bool falscheEingabe)
+        {
+            int eingabeID;
+            try
+            {
+                eingabeID = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                eingabeID = -1;
+                falscheEingabe = true;
+            }
+
+            if (!falscheEingabe && eingabeID >= 0 && eingabeID < Termine.ListenLaenge())
+            {
+                if (bearbeiten)
+                {
+                    Termine.TerminIDBestaetigung(eingabeID, 'b');
+                }
+                else
+                {
+                    Termine.TerminIDBestaetigung(eingabeID, 'l');
+                }
+            }
+            else
+            {
+                Console.CursorVisible = false;
+                HilfeText(5, 3);
+                FalscheEingabe(22, 8, 3);
+            }
+        }
+
         // Menü mit ID-Ausgabe wird jeweils gebraucht für die Löschung oder Bearbeitung von Terminen (Bearbeitung hat 2 Menüs vereint)
         static void TermineIDMenue(bool bearbeiten)
         {
-            string? eingabe;
-            int eingabeID;
-            bool falscheEingabe;
             do
             {
                 falscheEingabe = false;
                 Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("\n (I)D");
-                Console.ResetColor();
-                Console.WriteLine("\n\n\n           :\n\n     ID: \n\n\n Termine:\n");
+                Console.CursorVisible = false;
+                Console.Write($"{green}\n (I)ID{reset}");
+                
+                Console.WriteLine("\n\n\n\n\n     ID: \n\n\n Termine:\n");
                 Termine.TermineListeAusgabe(true);
 
                 Console.SetCursorPosition(55, 1);
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("(Z)urück");
-                Console.ResetColor();
+                Console.Write($"{blue}(Z)Zurück{reset}");
 
-                Console.SetCursorPosition(13, 4);
-                eingabe = Console.ReadLine();
-
-                if (eingabe != null) eingabe = eingabe.ToLower().Trim();
-
-                if (eingabe == "z")
+                switch (Console.ReadKey(true).Key)
                 {
-                    return;
-                }
-                else if (eingabe == "i")
-                {
-                    Console.SetCursorPosition(9, 6);
-                    try
-                    {
-                        eingabeID = Convert.ToInt32(Console.ReadLine());
-                    }
-                    catch
-                    {
-                        eingabeID = -1;
-                        falscheEingabe = true;
-                    }
+                    case ConsoleKey.Z:
+                        return;
+                    case ConsoleKey.I:
+                        Console.CursorVisible = true;
+                        Console.SetCursorPosition(9, 6);
+                        IDBearbeitung(bearbeiten, falscheEingabe);
+                        break;
 
-                    if (!falscheEingabe && eingabeID >= 0 && eingabeID < Termine.ListenLaenge())
-                    {
-                        if (bearbeiten)
-                        {
-                            //TermineEingabeMenue(true, eingabeID);
-                            Termine.TerminIDBestaetigung(eingabeID, 'b');
-                        }
-                        else
-                        {
-                            Termine.TerminIDBestaetigung(eingabeID, 'l');
-                        }
-                    }
-                    else
-                    {
-                        falscheEingabe = true;
-                    }
-                }
-                else
-                {
-                    falscheEingabe = true;
-                }
-
-                if (falscheEingabe)
-                {
-                    HilfeText(5, 3);
-                    FalscheEingabe(22, 8, 3);
                 }
 
             } while (true);
+        }
+
+        private static string UhrzeitEingabe(string eingabe)
+        {
+
+            falscheEingabe = true;
+            int stunden;
+            int minuten;
+            
+            stunden = IntPruefer();
+
+            if (stunden >= 0 && stunden <= 23)
+            {
+                minuten = IntPruefer();
+
+                if (minuten >= 0 && minuten < 60)
+                {
+                    eingabe += $" {stunden:00}:{minuten:00}";
+                    falscheEingabe = false;
+                }  
+                
+            }
+            return eingabe;
         }
 
         // Menü für die Erstellung oder Bearbeitung eines Termines (der zweite Teil der Bearbeitung)
         public static void TermineEingabeMenue(int id, char auswahl)
         {
             string? eingabe = null;
-            bool falscheEingabe = false;
             do
             {
                 if (falscheEingabe)
@@ -344,6 +306,7 @@ namespace KalenderUndTerminverwaltung
                 falscheEingabe = false;
                 Console.Clear();
                 KalenderAusdruck();
+
                 if (eingabe == null)
                 {
                     Console.Write("\nTag: ");
@@ -376,45 +339,9 @@ namespace KalenderUndTerminverwaltung
                 if (!falscheEingabe)
                 {
                     Console.Write("\nUhrzeit:   :  ");
-                    int stunden;
-                    int minuten;
-                    try
-                    {
-                        Console.SetCursorPosition(9, 13);
-                        stunden = Convert.ToInt32(Console.ReadLine());
 
-                    }
-                    catch
-                    {
-                        falscheEingabe = true;
-                        stunden = -1;
-                    }
-
-                    if (falscheEingabe || !falscheEingabe && stunden < 0 && stunden > 23 )
-                    {
-                        falscheEingabe = true;
-                        continue;
-                    }
-
-                    try
-                    {
-                        Console.SetCursorPosition(12, 13);
-                        minuten = Convert.ToInt32(Console.ReadLine());
-                    }
-                    catch
-                    {
-                        minuten = -1;
-                        falscheEingabe = true;
-                    }
-
-                    if (!falscheEingabe &&  minuten >= 0 && minuten < 60)
-                    {
-                        eingabe += $" {stunden:00}:{minuten:00}";
-                    }
-                    else
-                    {
-                        falscheEingabe = true;
-                    }
+                    Console.SetCursorPosition(9, 13);
+                    eingabe = UhrzeitEingabe(eingabe);
                 }
 
             } while (falscheEingabe);
@@ -445,9 +372,7 @@ namespace KalenderUndTerminverwaltung
         static void FalscheEingabe(int cursor, int zeile, int secunden)
         {
             Console.SetCursorPosition(cursor, zeile);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Falsche Eingabe");
-            Console.ResetColor();
+            Console.WriteLine($"{red}Falsche Eingabe{reset}");
             Thread.Sleep(secunden * 1000);
         }
     }
